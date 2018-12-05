79a830ec634bdd2315d585962cef9adae86618fd
Kevin Taylor <kevtaylor@expedia.com>
2018-12-05 12:29:57 +0000

    Add flag to docker

--- examples/kubernetes/kev/docker/build.sh
+++ examples/kubernetes/kev/docker/build.sh
@@ -1,4 +1,4 @@
 docker build -t osprey:dev .
-docker run --rm -v /root/.kube/kt:/root/.kube -i osprey:dev user login
+docker run --rm -v /root/.kube/kt:/root/.kube -it osprey:dev user login
 kubectl --kubeconfig /root/.kube/kt/kubeconfig --context kt get pods -n kube-public
 kubectl --kubeconfig /root/.kube/kt/kubeconfig --context kt get pods -n kube-system
